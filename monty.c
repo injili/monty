bce6509ef4f4fdfddfd3c23ae57e1da859420554
injili <nyarekigospel@gmail.com>
2023-03-23 12:10:53 -0700

    the main file

--- monty.c
+++ monty.c
@@ -46,4 +46,19 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 		error_usage();
+
+	file = fopen(argv[1], "r");
+	if (!file)
+		file_error(argv[1]);
+
+	while(1)
+	{
+		if (status)
+			break;
+		if (*buffer == '\n')
+		{
+			line_cnt++;
+			continue;
+		}
+	}
 }
