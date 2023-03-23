dc3907ada09cb0b67f64b9f4798a02ceb5e5b781
injili <nyarekigospel@gmail.com>
2023-03-23 05:19:27 -0700

    main file

---
+++ monty.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+/*global declarations*/
+int status = 0;
+
+/**
+ * file_error - prints file error message and exits
+ * @argv: given by main()
+ *
+ * Return: no return value
+ */
+
+void file_error(char *argv)
+{
+	fprintf();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * error_usage - prints usage message and exits
+ *
+ * Return: return nothing receive nothing
+ */
+
+void error_usage(void)
+{
+	fprintf();
+	exit(EXIT_FAILURE);
+}
+int status = 0;
+
+/**
+ * main - beginning of the code
+ * @argv: list of arguments passed to the function
+ * @argc: arguments pssed to the function
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+
+}
